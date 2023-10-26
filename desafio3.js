e92f9e40c3078ed5e26577121de44ac11b14615b
KEVEN KLYNSMAN DA PAIXAO BARATA <106642991+kevenklynsman@users.noreply.github.com>
2023-10-26 15:42:11 -0300

    desafio 3 adicionado

---
+++ desafio3.js
@@ -0,0 +1,39 @@
+// Classe Heroi
+class Heroi {
+    constructor(nome, idade, tipo) {
+      this.nome = nome;
+      this.idade = idade;
+      this.tipo = tipo;
+    }
+  
+    atacar() {
+      let ataque = "";
+  
+      switch (this.tipo) {
+        case "mago":
+          ataque = "usou magia";
+          break;
+        case "guerreiro":
+          ataque = "usou espada";
+          break;
+        case "monge":
+          ataque = "usou artes marciais";
+          break;
+        case "ninja":
+          ataque = "usou shuriken";
+          break;
+        default:
+          ataque = "usou um ataque desconhecido";
+      }
+  
+      console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+  }
+  
+  // Exemplo de uso da classe Heroi
+  const meuHeroi = new Heroi("Keven", 25, "mago");
+  meuHeroi.atacar();
+  
+  const outroHeroi = new Heroi("Lucas", 30, "guerreiro");
+  outroHeroi.atacar();
+  
